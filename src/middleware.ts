5684504e037536fc331a7820dfd94d650ca657a2
salmansheri <sheriffsalman00@gmail.com>
2024-12-07 10:48:56 +0530

    Fix: Middleware(3)

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,20 +3,27 @@ import type { Session } from "better-auth/types";
 import { NextResponse, type NextRequest } from "next/server";
 
 export default async function authMiddleware(request: NextRequest) {
+  const url = new URL(request.url);
   const response = NextResponse.next();
-  const { data: session } = await betterFetch<Session>(
-    "/api/auth/get-session",
-    {
-      baseURL: "https://write-it-by-salman.vercel.app",
-      headers: {
-        //get the cookie from the request
-        cookie: request.headers.get("cookie") || "",
+  try {
+    const { data: session } = await betterFetch<Session>(
+      "/api/auth/get-session",
+      {
+        baseURL: "https://write-it-by-salman.vercel.app",
+        headers: {
+          //get the cookie from the request
+          cookie: request.headers.get("cookie") || "",
+        },
+        credentials: "include",
       },
-    },
-  );
+    );
 
-  if (!session) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
+    if (!session) {
+      return NextResponse.redirect(new URL("/sign-in", url.origin));
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.redirect(new URL("/sign-in", url.origin));
   }
   return response;
 }
