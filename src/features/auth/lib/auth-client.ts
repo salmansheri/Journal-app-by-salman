35c197dd37c2b96a927abc9aa424ffc0ca7c2e93
salmansheri <sheriffsalman00@gmail.com>
2024-12-08 16:57:59 +0530

    Fix: Auth-client.ts

--- src/features/auth/lib/auth-client.ts
+++ src/features/auth/lib/auth-client.ts
@@ -1,4 +1,5 @@
+import { AppURL } from "@/lib/utils";
 import { createAuthClient } from "better-auth/react";
 export const client = createAuthClient({
-  baseURL: "http://localhost:3000", // the base url of your auth server
+  baseURL: AppURL, // the base url of your auth server
 });
